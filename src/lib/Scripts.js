6fedd8e756c7b4e8a13bdef824ff5fb3024d86d5
Mark Holtzhausen <mark.holtzhausen@superbalist.com>
2019-03-08 11:26:37 +0200

    Ensure script file exists

--- src/lib/Scripts.js
+++ src/lib/Scripts.js
@@ -3,22 +3,25 @@ const fs = require('fs-extra')
 const FILENAME = path.resolve(__dirname, '../../', 'data/scripts.json')
 
 class Scripts {
-  static get() {
-    let data = {}
-    if (fs.existsSync(FILENAME)) {
-      try {
-        data = require(FILENAME)
-      } catch (e) {}
+    static get() {
+        let data = {}
+        if (fs.existsSync(FILENAME)) {
+            try {
+                data = require(FILENAME)
+            } catch (e) {}
+        }
+        return data
     }
-    return data
-  }
 
-  static set(data) {
-    fs.writeFileSync(FILENAME, JSON.stringify(data, null, 2))
-    try {
-      delete require.cache[require.resolve(FILENAME)]
-    } catch (e) {}
-  }
+    static set(data) {
+        fs.createFile(FILENAME, err => {
+            if (err) throw new Error(err)
+            fs.writeFileSync(FILENAME, JSON.stringify(data, null, 2))
+        })
+        try {
+            delete require.cache[require.resolve(FILENAME)]
+        } catch (e) {}
+    }
 }
 
 module.exports = Scripts
